4ce5f422af0333b82bd20e560b8550409b509e09
waylandli <wli108@ucsc.edu>
2021-03-29 21:09:04 -0700

    Added tempoross and packages

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def get_hiscore(name):
                 'Commander Zilyana', 'Corporeal Beast', 'Crazy Archaeologist', 'Dagannoth Prime', 'Dagannoth Rex',
                 'Dagannoth Supreme', 'Deranged Archaeologist', 'General Graardor', 'Giant Mole', 'Grotesque Guardians',
                 'Hespori', 'Kalphite Queen', 'King Black Dragon', 'Kraken', "Kree'Arra", "K'ril Tsutsaroth", 'Mimic', 'Nightmare',
-                'Obor', 'Sarachnis', 'Scorpia', 'Skotizo', 'The Gauntlet', 'The Corrupted Gauntlet', 'Theatre of Blood',
+                'Obor', 'Sarachnis', 'Scorpia', 'Skotizo', 'Tempoross', 'The Gauntlet', 'The Corrupted Gauntlet', 'Theatre of Blood',
                 'Thermonuclear Smoke Devil', 'TzKal-Zuk', 'TzTok-Jad', 'Venenatis', "Vet'ion", 'Vorkath', 'Wintertodt',
                 'Zalcano', 'Zulrah']
 
