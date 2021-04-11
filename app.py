45c36a8f803d3ea3dced96eea556b601c2e15d16
waylandli <wli108@ucsc.edu>
2021-04-11 01:27:54 -0700

    Added cors

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restful import abort
+from flask_cors import CORS
 import requests
 import csv
 import io
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/<name>")
